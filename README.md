a856d6eb224896925eecf5edc7ebfc688a366545
tphiepbk <itmanagertph@gmail.com>
2021-05-31 23:48:40 +0700

    Added : test_send_data small app and README file

---
+++ README.md
@@ -0,0 +1,22 @@
+# Smart Farm System Project
+---
+
+## Technologies
+* NodeJs (server)
+* ExpressJs (web app)
+* Socket.io (transfer data from server to client)
+* MongoDB (database)
+
+## Features
+* Turn on/off light system and water pump system automtically (based on the data fetched from database)
+* Draw graphs representing current soil and light values
+
+<ins>***Note***</ins>: Use the small web app in folder "test_send_data" to send data to Adafruit Server
+
+## About
+
+***Powered by FWB Team***:
+* Thai Phuc Hiep (main coder)
+* Lam Thanh Huy
+* Le Chung Hoang
+* Nguyen Trong Hoang
---
+++ test_send_data/test_send_data.js
@@ -0,0 +1,69 @@
+var port        = 443;
+var host        = "io.adafruit.com";
+var username    = "tphiepbk";
+var password    = "aio_vjlb21Jsae7D86XwPisWl5WVvud7";
+
+var topicSoil = "tphiepbk/feeds/bk-iot-soil";
+var topicLight = "tphiepbk/feeds/bk-iot-light";
+
+var client;
+
+// send a message
+function publishSoilData () {
+
+    var value = parseInt(document.getElementById('soil_value').value);
+    console.log(value);
+
+    if (value < 0) value = 0;
+    if (value > 1023) value = 1023;
+
+    var message = JSON.stringify({"id" : "9", "name": "SOIL", "data" : value.toString() , "unit" : ""});
+    client.send(topicSoil, message);
+}
+
+function publishLightData () {
+
+    var value = parseInt(document.getElementById('light_value').value);
+    console.log(value);
+
+    if (value < 0) value = 0;
+    if (value > 1023) value = 1023;
+
+    var message = JSON.stringify({"id" : "13", "name": "LIGHT", "data" : value.toString() , "unit" : ""});
+    client.send(topicLight, message);
+}
+
+function init() {
+    console.log("Connecting...");
+    client = new Paho.MQTT.Client(host, Number(port), "myclientid_" + parseInt(Math.random() * 100, 10));
+
+    client.onConnectionLost = onConnectionLost;
+    client.onMessageArrived = onMessageArrived;
+
+    client.connect({ 
+        password : password,
+        userName: username,
+        onSuccess: onConnect 
+    });
+}
+
+// called when the client connects
+function onConnect() {
+    client.subscribe(topicSoil);
+    client.subscribe(topicLight);
+    console.log("Connect successfully");
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+        console.log("onConnectionLost:" + responseObject.errorMessage);
+    }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+    console.log("onMessageArrived:" + message.payloadString);
+}
+
+init();
