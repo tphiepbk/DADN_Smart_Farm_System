826a01541b47d93febb9248cbb8600e614b44534
tphiepbk <itmanagertph@gmail.com>
2021-06-04 13:49:58 +0700

    Created new database for group , changed the connection string

--- db/get_data_test.js
+++ db/get_data_test.js
@@ -4,7 +4,8 @@ const assert = require("assert");
 const MongoClient = require("mongodb").MongoClient;
 
 // Connection string
-const connectionString = "mongodb+srv://tphiepbk:hiepit-2992@cluster0.axbkf.mongodb.net/bk-iot-test?retryWrites=true&w=majority";
+//const connectionString = "mongodb+srv://tphiepbk:hiepit-2992@cluster0.axbkf.mongodb.net/bk-iot-test?retryWrites=true&w=majority";
+const connectionString = "mongodb+srv://fwbteam:fwbteam@cluster0.in5dd.mongodb.net/bk-iot?retryWrites=true&w=majority";
 
 /*
 const light_relay_url = "https://io.adafruit.com/api/v2/tphiepbk/feeds/bk-iot-light-relay/data.json?X-AIO-Key=aio_vjlb21Jsae7D86XwPisWl5WVvud7"
@@ -14,10 +15,10 @@ const water_pump_relay_url = "https://io.adafruit.com/api/v2/tphiepbk/feeds/bk-i
 */
 
 // Real devices
-const light_relay_url = "https://io.adafruit.com/api/v2/CSE_BBC1/feeds/bk-iot-relay/data.json?X-AIO-Key=aio_sRyV27Jw7nbPzH7V8GaZ9lFlnK3Y"
-const light_url = "https://io.adafruit.com/api/v2/CSE_BBC1/feeds/bk-iot-light/data.json?X-AIO-Key=aio_sRyV27Jw7nbPzH7V8GaZ9lFlnK3Y"
-const soil_url = "https://io.adafruit.com/api/v2/CSE_BBC/feeds/bk-iot-soil/data.json?X-AIO-Key=aio_HmqE80NeMr7TJFY6dyfuGNrR81R5"
-const water_pump_relay_url = "https://io.adafruit.com/api/v2/CSE_BBC1/feeds/bk-iot-relay/data.json?X-AIO-Key=aio_sRyV27Jw7nbPzH7V8GaZ9lFlnK3Y"
+const light_relay_url = "https://io.adafruit.com/api/v2/CSE_BBC1/feeds/bk-iot-relay/data.json?X-AIO-Key=aio_wXPC09rE51bD1CFHcZnaBmTzeFiW"
+const light_url = "https://io.adafruit.com/api/v2/CSE_BBC1/feeds/bk-iot-light/data.json?X-AIO-Key=aio_wXPC09rE51bD1CFHcZnaBmTzeFiW"
+const soil_url = "https://io.adafruit.com/api/v2/CSE_BBC/feeds/bk-iot-soil/data.json?X-AIO-Key=aio_aaXQ56Mtv3RWWwps1wWDPCWdq8S6"
+const water_pump_relay_url = "https://io.adafruit.com/api/v2/CSE_BBC1/feeds/bk-iot-relay/data.json?X-AIO-Key=aio_wXPC09rE51bD1CFHcZnaBmTzeFiW"
 
 function upload(collectionName, data) {
     MongoClient.connect(connectionString, function(err, client) {
