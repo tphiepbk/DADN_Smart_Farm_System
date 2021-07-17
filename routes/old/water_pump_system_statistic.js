b6b90be53dc1b4899c87373ef5bb183e6d91a16a
tphiepbk <itmanagertph@gmail.com>
2021-07-17 23:44:41 +0700

    Refactor code : remove unnecessary file

--- routes/old/water_pump_system_statistic.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-router.get('/', function(req, res) {
-    res.render('water_pump_system_statistic');
-});
-
-module.exports = router;
